4012ccf0c331af078649ff4b73bef7e347e0880e
saurav278 <52128941+saurav278@users.noreply.github.com>
2020-06-22 01:46:00 +0530

    Delete Assignment 4.6.py

--- Assignment/Assignment 4.6.py
+++
@@ -1,8 +0,0 @@
-def computepay():
-    h = int(input("use hours"))
-    r = float(input("rate per hour"))
-    if h > 40:
-        return 40*r+ (h-40)*r*1.5
-    else:
-        return h*r
-print ("Pay",computepay())
